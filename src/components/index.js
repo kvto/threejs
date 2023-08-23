acb32d5d3d785914653b1a1cb1053863f174e403
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-08-23 17:33:30 -0500

    begin

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,14 @@
 import CustomButton from "./CustomButton";
+import AlPicker from "./CustomButton";
+import ColorPicker from "./CustomButton";
+import FilePicker from "./CustomButton";
+import Tab from "./CustomButton";
 
-export {CustomButton}
+
+export {
+    CustomButton,
+    AlPicker,
+    ColorPicker,
+    FilePicker,
+    Tab
+}
--- src/page/Customizer.jsx
+++ src/page/Customizer.jsx
@@ -1,4 +1,13 @@
+import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
 
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, filterTabs, DecalTypes} from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
 const Customizer = () => {
   return (
     <div>Customizer</div>
