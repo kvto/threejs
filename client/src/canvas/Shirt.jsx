c5d851635672338333855fec8d5d84a7e0d7e7eb
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-09-04 17:26:01 -0500

    begin

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/no-unknown-property */
-import React from 'react'
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
